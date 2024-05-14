1803b0720646e3e172ba056dcb490dcd7a74a797
RodrigoS <2005rodrigosilva@gmail.com>
2024-05-14 20:27:56 -0300

    Create services of Tanque
    Criando services e CRUD do tanque e exportando serviço.

--- services/TanqueService.js
+++ services/TanqueService.js
@@ -3,16 +3,42 @@ import mongoose from "mongoose"
 
 const Tanque = mongoose.Model("Tanque", tanque)
 
-class TanqueService{
+class TanqueService {
+    //Consultar todos os tanques
+    SelectAll() {
+        const tanque = Tanque.find()
+        return tanques
+    }
 
+    // Cadastrar um Novo Tanque
     Create(nome, capacidade) {
         const newTanque = new Tanque({
-            nome : nome,
-            capacidade : capacidade
+            nome: nome,
+            capacidade: capacidade
         })
         newTanque.save()
     }
-    
+
+    //EXCLUIR um Tanque
+    Delete(nome) {
+        User.findByIdAndDelete(nome).then(() => {
+            console.log(`Tanque com nome "${nome}" foi deletado do sistema.`)
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
+    // ALTERAR
+    Update(nome, capacidade) {
+        User.findByIdAndUpdate(nome, {
+            nome: nome,
+            capacidade: capacidade
+        }).then(() => {
+            console.log(`Tanque com nome "${nome}" alterado com sucesso`)
+        }).catch(err => {
+            console.log(err)
+        })
+    }
 }
 
 export default new TanqueService()
