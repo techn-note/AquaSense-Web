aaee10f6579e21cddf785a4f3ea6c6b6022464bf
RodrigoS <2005rodrigosilva@gmail.com>
2024-05-20 19:26:04 -0300

    Update SensoresService.js
    Adicionando seleções únicas dos tipos de sensores para tratar os dados por fora.

--- services/SensoresService.js
+++ services/SensoresService.js
@@ -20,11 +20,36 @@ class SensoresService {
         newSensores.save();
     }
 
-    // Selecionar sensores por tipo
-    async selectByType(tipo) {
-        const sensores = await Sensores.find({ tipo: tipo });
-        return sensores;
+    // Selecionar o último valor inserido por tipo - PH
+    async selectPh() {
+        const sensorPh = await Sensores.findOne({ tipo: 'ph' }).sort({ data: -1 });
+        return sensorPh;
     }
+
+    // Selecionar o último valor inserido por tipo - TEMPERATURA
+    async selectTemp() {
+        const sensorTemp = await Sensores.findOne({ tipo: 'temperatura' }).sort({ data: -1 });
+        return sensorTemp;
+    }
+
+    // Selecionar o último valor inserido por tipo - AMONIA
+    async selectAmonia() {
+        const sensorAmonia = await Sensores.findOne({ tipo: 'amonia' }).sort({ data: -1 });
+        return sensorAmonia;
+    }
+
+    // Selecionar o último valor inserido por tipo - OXIGENAÇÃO
+    async selectOxigenacao() {
+        const sensorOxigenacao = await Sensores.findOne({ tipo: 'oxigenacao' }).sort({ data: -1 });
+        return sensorOxigenacao;
+    }
+
+    // Selecionar o último valor inserido por tipo - VOLUME
+    async selectVolume() {
+        const sensorVolume = await Sensores.findOne({ tipo: 'volume' }).sort({ data: -1 });
+        return sensorVolume;
+    }
+
 }
 
 export default new SensoresService();
